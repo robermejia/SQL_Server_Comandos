eefa0a076d764d237f7470a053217ede6ad7f2c9
Roberto Mejía <robertomejiacollazos@gmail.com>
2024-06-06 20:33:02 -0500

    Modificando la clausula where

--- 8_Clausula_Where.sql
+++ 8_Clausula_Where.sql
@@ -1,4 +1,4 @@
---CLAUSULA WHERE
+--CLAUSULA WHERE (seleciona los campos que cumplan con la condición)
 SELECT * FROM Empleado WHERE nombre = 'Jorge';
 SELECT * FROM Empleado WHERE edad = 29;
 SELECT edad FROM Empleado WHERE edad = 29;
