41d9c0e378c208129bcb45545b48cd3ae4d26c61
Roberto Mejía <robertomejiacollazos@gmail.com>
2024-06-10 16:58:48 -0500

    Subiendo cambios 1 10/06/24

--- 4_Crear_Añadir_Campo_Registro_y_Tabla.sql
+++ 4_Crear_Añadir_Campo_Registro_y_Tabla.sql
@@ -15,7 +15,10 @@ CREATE TABLE Empleados( --CREATE TABLE nombre_de_tabla
 INSERT INTO Empleados VALUES(1, 'José', 'Feliciano', 35, 1234567890, 'Calle primavera n1', '2024-06-03', 30000.00, 'SI');        
 INSERT INTO Empleados VALUES(2, 'Manuel', 'Perez', 30, 9234567890, 'Calle Cotosh n1', '2024-08-10', 20000.00, 'NO');
                         -- id, nombre, apellido, edad, telefono, direccion, fecha, salario, activo
-
+                        
+-- AÑADIR CAMPO EN TABLA (tabla vacia o en uso)
+ALTER TABLE Empleados ADD sexo CHAR(1);
+--ALTER TABLE Nombre_Tabla ADD Nuevo_Campo TIPO_DE_DATO(Cant. de caracteres)
 
 
 
